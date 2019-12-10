a3f6dd8365fbf96b2b6e1c692d94bded37009b6e
Mary Luna <jimenaluna84@gmail.com>
2019-12-10 17:44:03 -0400

    MyLinkedList

--- src/data_structures/MyLinkedList.java
+++ src/data_structures/MyLinkedList.java
@@ -11,6 +11,12 @@ public class MyLinkedList<T> implements List<T> {
     private Node last;
     private Integer size;
 
+    MyLinkedList() {
+        first = null;
+        last = null;
+        size = 0;
+    }
+
     public Node getFirst() {
         return first;
     }
@@ -79,180 +85,186 @@ public class MyLinkedList<T> implements List<T> {
     }
 
 
-    public static MyLinkedList deleteByKey(MyLinkedList list, String key) {
-        // Store head node
-        Node currNode = list.first, prev = null;
-
-        //
-        // CASE 1:
-        // If head node itself holds the key to be deleted
-
-        if (currNode != null && currNode.getValue() == key) {
-            list.first = currNode.getNext(); // Changed head
-
-            // Display the message
-            System.out.println(key + " found and deleted");
-
-            // Return the updated List
-            return list;
-        }
-
-        //
-        // CASE 2:
-        // If the key is somewhere other than at head
-        //
-
-        // Search for the key to be deleted,
-        // keep track of the previous node
-        // as it is needed to change currNode.next
-        while (currNode != null && currNode.getValue() != key) {
-            // If currNode does not hold key
-            // continue to next node
-            prev = currNode;
-            currNode = currNode.getNext();
-        }
-
-        // If the key was present, it should be at currNode
-        // Therefore the currNode shall not be null
-        if (currNode != null) {
-            // Since the key is at currNode
-            // Unlink currNode from linked list
-            prev.setNext(currNode.getNext());
-
-            // Display the message
-            System.out.println(key + " found and deleted");
-        }
-
-        //
-        // CASE 3: The key is not present
-        //
-
-        // If key was not present in linked list
-        // currNode should be null
-        if (currNode == null) {
-            // Display the message
-            System.out.println(key + " not found");
-        }
-
-        // return the List
-        return list;
-    }
 
 
     @Override
-        public int size () {
-            return 0;
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        Node node = first;
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean add(T node) {
+        public static MyLinkedList deleteByKey(MyLinkedList list, String key) {
+            // Store head node
+            Node currNode = list.first, prev = null;
+
+            //
+            // CASE 1:
+            // If head node itself holds the key to be deleted
+
+            if (currNode != null && currNode.getValue() == key) {
+                list.first = currNode.getNext(); // Changed head
+
+                // Display the message
+                System.out.println(key + " found and deleted");
+
+                // Return the updated List
+                return list;
+            }
+
+            //
+            // CASE 2:
+            // If the key is somewhere other than at head
+            //
+
+            // Search for the key to be deleted,
+            // keep track of the previous node
+            // as it is needed to change currNode.next
+            while (currNode != null && currNode.getValue() != key) {
+                // If currNode does not hold key
+                // continue to next node
+                prev = currNode;
+                currNode = currNode.getNext();
+            }
+
+            // If the key was present, it should be at currNode
+            // Therefore the currNode shall not be null
+            if (currNode != null) {
+                // Since the key is at currNode
+                // Unlink currNode from linked list
+                prev.setNext(currNode.getNext());
+
+                // Display the message
+                System.out.println(key + " found and deleted");
+            }
+
+            //
+            // CASE 3: The key is not present
+            //
+
+            // If key was not present in linked list
+            // currNode should be null
+            if (currNode == null) {
+                // Display the message
+                System.out.println(key + " not found");
+            }
+
+            // return the List
+            return list;
         }
 
-        @Override
-        public boolean isEmpty () {
-            return false;
-        }
 
-        @Override
-        public boolean contains (Object o){
-            return false;
-        }
 
-        @Override
-        public Iterator iterator () {
-            return null;
-        }
 
-        @Override
-        public Object[] toArray () {
-            return new Object[0];
-        }
+        return false;
+    }
 
-        @Override
-        public boolean add (Object o){
-            return false;
-        }
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
 
-        @Override
-        public boolean remove (Object o){
-            return false;
-        }
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
 
-        @Override
-        public boolean addAll (Collection c){
-            return false;
-        }
+    @Override
+    public boolean addAll(int index, Collection c) {
+        return false;
+    }
 
-        @Override
-        public boolean addAll ( int index, Collection c){
-            return false;
-        }
+    @Override
+    public void clear() {
 
-        @Override
-        public void clear () {
+    }
 
-        }
+    @Override
+    public T get(int index) {
+        return null;
+    }
 
-        @Override
-        public T get ( int index){
-            return null;
-        }
+    @Override
+    public Object set(int index, Object element) {
+        return null;
+    }
 
-        @Override
-        public Object set ( int index, Object element){
-            return null;
-        }
-
-        @Override
-        public void add ( int index, Object element){
-
-        }
-
-        @Override
-        public T remove ( int index){
-            return null;
-        }
-
-        @Override
-        public int indexOf (Object o){
-            return 0;
-        }
-
-        @Override
-        public int lastIndexOf (Object o){
-            return 0;
-        }
-
-        @Override
-        public ListIterator listIterator () {
-            return null;
-        }
-
-        @Override
-        public ListIterator listIterator ( int index){
-            return null;
-        }
-
-        @Override
-        public List subList ( int fromIndex, int toIndex){
-            return null;
-        }
-
-        @Override
-        public boolean retainAll (Collection c){
-            return false;
-        }
-
-        @Override
-        public boolean removeAll (Collection c){
-            return false;
-        }
-
-        @Override
-        public boolean containsAll (Collection c){
-            return false;
-        }
-
-        @Override
-        public Object[] toArray (Object[]a){
-            return new Object[0];
-        }
+    @Override
+    public void add(int index, Object element) {
 
 
     }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+
+
+}
--- tests/data_structures/MyLinkedListTest.java
+++ tests/data_structures/MyLinkedListTest.java
@@ -1,11 +1,59 @@
 package data_structures;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class MyLinkedListTest {
+    @Test
+    public void testAddMethodShouldIncreaseTheSizeOfTheList() {
+        MyLinkedList<String> myLinkedList = new MyLinkedList<>();
+
+        myLinkedList.add("sdsdf");
+        myLinkedList.add("dsdf");
+        myLinkedList.add("hello");
+
+        assertEquals(3, myLinkedList.size());
+    }
 
     @Test
-    public void test(){}
+    public void testToString() {
+        MyLinkedList<String> myLinkedList = new MyLinkedList<>();
 
-    
+        myLinkedList.add("world");
+        myLinkedList.add("hello");
+        myLinkedList.add("gat");
+        assertEquals("[world, hello, gat]", myLinkedList.toString());
+    }
+
+    @Test
+    public void testGetMethodShouldReturnTheValueOfTheNodeOnTheSpecifiedIndex() {
+        MyLinkedList<String> myLinkedList = new MyLinkedList<>();
+
+        myLinkedList.add("world");
+        myLinkedList.add("people");
+        myLinkedList.add("hello");
+
+        assertEquals("world", myLinkedList.get(0));
+        assertEquals("people", myLinkedList.get(1));
+        assertEquals("hello", myLinkedList.get(2));
+    }
+
+
+    @Test
+    public void testConstainsShouldReturnTrueIfExists() {
+        MyLinkedList<String> myLinkedList = new MyLinkedList<>();
+
+        myLinkedList.add("world");
+        myLinkedList.add("people");
+        myLinkedList.add("hello");
+
+        assertFalse(myLinkedList.contains("hola"));
+        assertTrue(myLinkedList.contains("people"));
+    }
 }
