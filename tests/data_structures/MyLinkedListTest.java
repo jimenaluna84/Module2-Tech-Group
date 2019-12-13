cedb7e018ca0fbbf30c1682ff380c4088e816e64
Mary Luna <jimenaluna84@gmail.com>
2019-12-13 18:55:15 -0400

    MyLinkedList

--- tests/data_structures/MyLinkedListTest.java
+++ tests/data_structures/MyLinkedListTest.java
@@ -2,9 +2,58 @@ package data_structures;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class MyLinkedListTest {
     @Test
-    public void test(){}
+    public void verifySize() {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add("Rojo");
+        myLinkedList.add("Amarillo");
+        myLinkedList.add("Azul");
+        myLinkedList.add("Verde");
+        myLinkedList.add("Blanco");
+        assertEquals(5, myLinkedList.size());
 
-    
+    }
+
+    @Test
+    public void verifyAddNode() {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add("Rojo");
+        myLinkedList.add("Amarillo");
+        myLinkedList.add("Azul");
+        myLinkedList.add("Verde");
+        myLinkedList.add("Blanco");
+        assertEquals("Rojo", myLinkedList.get(0));
+        assertEquals("Amarillo", myLinkedList.get(1));
+        assertEquals("Azul", myLinkedList.get(2));
+        assertEquals("Verde", myLinkedList.get(3));
+        assertEquals("Blanco", myLinkedList.get(4));
+
+    }
+    @Test
+    public void verifyDeleteNode() {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add("Rojo");
+        myLinkedList.add("Amarillo");
+        myLinkedList.add("Azul");
+        myLinkedList.add("Verde");
+        myLinkedList.add("Blanco");
+        assertEquals("Rojo", myLinkedList.remove(0));
+        assertEquals("Amarillo", myLinkedList.get(1));
+        assertEquals("Azul", myLinkedList.get(2));
+        assertEquals("Verde", myLinkedList.get(3));
+        assertEquals("Blanco", myLinkedList.get(4));
+
+    }
+
 }
