9b8ef2925025544022702bc2063ea75135301b73
Mary Luna <jimenaluna84@gmail.com>
2020-01-31 11:45:28 -0400

    Binary Tree

---
+++ src/data_structures/MyBinaryTree.java
@@ -0,0 +1,97 @@
+package data_structures;
+
+
+public class MyBinaryTree {
+    private NodeBT root;
+    private int level;
+    private int size;
+
+
+    public MyBinaryTree() {
+
+    }
+
+    public boolean add(int value) {
+        NodeBT newNode = new NodeBT(value);
+        boolean result;
+        if (root == null) {
+            root = newNode;
+            this.size++;
+            result = true;
+        } else {
+            result = add(root, newNode);
+            if (result) {
+                this.size++;
+            }
+        }
+        return result;
+    }
+
+    private boolean add(NodeBT cursor, NodeBT newNode) {
+        boolean result = false;
+        if (newNode.getValue() > cursor.getValue()) {
+            if (cursor.getRight() == null) {
+                cursor.setRight(newNode);
+                result = true;
+            } else {
+                return add(cursor.getRight(), newNode);
+            }
+        } else if (newNode.getValue() < cursor.getValue()) {
+            if (cursor.getLeft() == null) {
+                cursor.setLeft(newNode);
+                result = true;
+            } else {
+                return add(cursor.getLeft(), newNode);
+            }
+        }
+        return result;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("[");
+
+        visit(root, builder);
+
+        builder.append("]");
+        return builder.toString();
+    }
+
+    private void visit(NodeBT cursor, StringBuilder builder) {
+        if (cursor == null) return;
+
+        visit(cursor.getLeft(), builder);
+        builder.append(cursor.getValue());
+        visit(cursor.getRight(), builder);
+    }
+
+    public boolean contains(int value) {
+        if (this.root == null) {
+            return false;
+        }
+        return contains(this.root, value);
+    }
+
+    private boolean contains(NodeBT current, int value) {
+
+        if (current == null) {
+            return false;
+        }
+        if (value == current.getValue()) {
+            return true;
+        }
+        return value < current.getValue()
+                ? contains(current.getLeft(), value)
+                : contains(current.getRight(), value);
+
+    }
+}
+
---
+++ tests/data_structures/MyBinaryTreeTest.java
@@ -0,0 +1,67 @@
+package data_structures;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class MyBinaryTreeTest {
+    @Test
+    public void testAdd() {
+        MyBinaryTree myTree = new MyBinaryTree();
+
+        myTree.add(8);
+        myTree.add(10);
+        myTree.add(6);
+        myTree.add(2);
+        myTree.add(5);
+        myTree.add(7);
+        myTree.add(9);
+        myTree.add(3);
+
+//        assertEquals(3, myTree.getLevel());
+        assertEquals(8, myTree.size());
+    }
+
+    @Test
+    public void testToString() {
+        MyBinaryTree myTree = new MyBinaryTree();
+
+        myTree.add(8);
+        myTree.add(6);
+        myTree.add(10);
+        myTree.add(9);
+        myTree.add(3);
+        myTree.add(7);
+        myTree.add(2);
+        myTree.add(4);
+
+        assertEquals("[234678910]", myTree.toString());
+    }
+
+
+    @Test
+    public void Contains() {
+        MyBinaryTree myTree = new MyBinaryTree();
+
+        myTree.add(8);
+        myTree.add(10);
+        myTree.add(6);
+        myTree.add(2);
+        myTree.add(5);
+        myTree.add(7);
+        myTree.add(9);
+        myTree.add(3);
+
+        assertTrue(myTree.contains(8));
+        assertTrue(myTree.contains(10));
+        assertTrue(myTree.contains(6));
+        assertTrue(myTree.contains(2));
+        assertTrue(myTree.contains(5));
+        assertTrue(myTree.contains(7));
+        assertTrue(myTree.contains(9));
+        assertTrue(myTree.contains(3));
+    }
+
+
+}
